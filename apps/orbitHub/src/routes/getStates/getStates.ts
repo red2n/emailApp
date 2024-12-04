8bae9944b09606b86169636000c5cc57ac71e7da
red2n <ksnavinkumar.diary@gmail.com>
2024-12-04 12:45:48 +0000

    fixed route return

--- apps/orbitHub/src/routes/getStates/getStates.ts
+++ apps/orbitHub/src/routes/getStates/getStates.ts
@@ -1,7 +1,7 @@
 
 import { InboundSyns, HttpMethod, ROUTETYPE } from '@modules/starter';
 
-export class GetStates implements InboundSyns<Request, string[], string[], Response> {
+export class GetStates implements InboundSyns<Request, string[], string[], string> {
     ID = 'GET_STATES';
     ROUTE_URL = '/states';
     METHOD = HttpMethod.GET;
@@ -9,11 +9,7 @@ export class GetStates implements InboundSyns<Request, string[], string[], Respo
     extract = (request: Request): string[] => {
         return ["Tamil Nadu", "Kerala", "Andra Pradesh"];
     };
-    respond = (response: string[]): Response => {
-        const responseObj = new Response(JSON.stringify(response), {
-            status: 200,
-            headers: { 'Content-Type': 'application/json' }
-        });
-        return responseObj;
-    };
+    respond = (response: string[]): string => {
+        return JSON.stringify(response);
+    }
 }
