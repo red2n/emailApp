789b633c36e7fda09fb8bf4e46c85466df5c6c31
red2n <ksnavinkumar.diary@gmail.com>
2024-12-04 10:17:15 +0000

    added documentation

--- modules/starter/src/routes/inboundSync.ts
+++ modules/starter/src/routes/inboundSync.ts
@@ -1,4 +1,18 @@
 import { Route } from "./mapper.js";
+/**
+ * Represents an inbound synchronization route with specific types for input, return, response, and output.
+ *
+ * @template INTERNALIN - The type of the input request.
+ * @template INTERNALRETURN - The type of the data returned from the extract function.
+ * @template INTERNALRES - The type of the data returned from the process function.
+ * @template INTERNALOUT - The type of the output response.
+ *
+ * @extends Route
+ *
+ * @property {function(INTERNALIN): INTERNALRETURN} extract - A function that extracts data from the input request.
+ * @property {function(INTERNALRETURN): INTERNALRES} [process] - An optional function that processes the extracted data.
+ * @property {function(INTERNALRES): INTERNALOUT} respond - A function that generates the output response from the processed data.
+ */
 export type InboundSyns<INTERNALIN,INTERNALRETURN,INTERNALRES,INTERNALOUT>  = Route &{
     extract: (request:INTERNALIN)=>INTERNALRETURN;
     process?: (data:INTERNALRETURN)=>INTERNALRES;
