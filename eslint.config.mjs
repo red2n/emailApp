f4f353f1af12c40798f948c8321eefd582a14d78
red2n <ksnavinkumar.diary@gmail.com>
2024-12-05 12:20:31 +0000

    initialize repo

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,4 @@
 import globals from "globals";
-import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 
 
@@ -7,6 +6,5 @@ import tseslint from "typescript-eslint";
 export default [
   {files: ["**/*.{js,mjs,cjs,ts}"]},
   {languageOptions: { globals: globals.node }},
-  pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
 ];
