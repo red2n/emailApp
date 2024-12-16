66500bbc9156aac8fde5d08c8523d1c38414172f
red2n <ksnavinkumar.diary@gmail.com>
2024-12-16 05:19:36 +0000

    initialize repo

--- apps/orbitHub/src/app.ts
+++ apps/orbitHub/src/app.ts
@@ -1,5 +1,5 @@
 import {
-  AppLogger, AppServer, type HttpBase, type InboundSyns, KafkaEssentials, KafkaUtils, MongoEssentials
+  AppLogger, AppServer, KafkaEssentials, KafkaUtils, MongoEssentials
 } from '@modules/starter';
 import dotenv from 'dotenv';
 import type { FastifyInstance } from 'fastify';
@@ -8,84 +8,87 @@ import type { Collection } from 'mongodb';
 import { routes } from './routes/register.js';
 import { getConnectionString } from './utils.js';
 
-const SERVICE_NAME = 'App';
-const DEFAULT_PORT = '8080';
-const DB_COLLECTION_NAME = 'rGuestStay';
-const SHUTTING_DOWN_MESSAGE = 'Shutting down gracefully...';
-const SERVER_CLOSED_MESSAGE = 'Server closed';
-const FIRST_DOCUMENT_MESSAGE = 'First document in rGuestStay collection:';
-const consumers: Consumer[] = [];
-const producers: Producer[] = [];
-dotenv.config();
-const PORT: number = Number.parseInt(process.env.PORT || DEFAULT_PORT);
-const app: FastifyInstance = AppLogger.getLogger(SERVICE_NAME);
+class App {
+  private static readonly SERVICE_NAME = 'App';
+  private static readonly DEFAULT_PORT = '8080';
+  private static readonly DB_COLLECTION_NAME = 'rGuestStay';
+  private static readonly SHUTTING_DOWN_MESSAGE = 'Shutting down gracefully...';
+  private static readonly SERVER_CLOSED_MESSAGE = 'Server closed';
+  private static readonly FIRST_DOCUMENT_MESSAGE = 'First document in rGuestStay collection:';
+  private consumers: Consumer[] = [];
+  private producers: Producer[] = [];
+  private app: FastifyInstance;
+  private rGuestStayCollection: Collection | undefined;
 
-AppServer.setupFastify(app);
-
-const registerHttpRoutes = async () => {
-  for (const route of routes) {
-    route.initialize(app);
+  constructor() {
+    dotenv.config();
+    const PORT: number = Number.parseInt(process.env.PORT || App.DEFAULT_PORT);
+    this.app = AppLogger.getLogger(App.SERVICE_NAME);
+    AppServer.setupFastify(this.app);
+    this.initialize(PORT);
+    this.setupGracefulShutdown();
   }
-};
 
-let rGuestStayCollection: Collection;
+  private async registerHttpRoutes() {
+    for (const route of routes) {
+      route.initialize(this.app);
+    }
+  }
 
-const initialize = async () => {
-  try {
-    const connectionString = getConnectionString();
-    const client = await MongoEssentials.connectToMongoDB(connectionString);
-    const db = client.db(process.env.DB_NAME);
-    rGuestStayCollection = db.collection(DB_COLLECTION_NAME);
-    rGuestStayCollection.findOne({}).then((result) => {
-      const doc = result?._id;
-      if (doc) {
-        app.log.info(`${FIRST_DOCUMENT_MESSAGE} ${doc}`);
-      }
-      else {
-        app.log.warn(`${FIRST_DOCUMENT_MESSAGE} not found`);
+  private async initialize(PORT: number) {
+    try {
+      const connectionString = getConnectionString();
+      const client = await MongoEssentials.connectToMongoDB(connectionString);
+      const db = client.db(process.env.DB_NAME);
+      this.rGuestStayCollection = db.collection(App.DB_COLLECTION_NAME);
+      this.rGuestStayCollection.findOne({}).then((result) => {
+        const doc = result?._id;
+        if (doc) {
+          this.app.log.info(`${App.FIRST_DOCUMENT_MESSAGE} ${doc}`);
+        } else {
+          this.app.log.warn(`${App.FIRST_DOCUMENT_MESSAGE} not found`);
+        }
+      });
+      await Promise.all([
+        KafkaEssentials.connectToKafka().then(() => {
+          this.registerHttpRoutes().then(() => {
+            AppServer.startFastify(this.app, PORT);
+          }).catch((err) => {
+            this.app.log.error('Error registering routes:', err);
+            process.exit(1);
+          });
+        }).catch((err) => {
+          this.app.log.error(`Error Connecting Kafka: ${err}`);
+          process.exit(1);
+        }),
+      ])
+        .then(() => {
+          this.app.log.info('All services started successfully');
+        })
+        .catch((err) => {
+          this.app.log.error('Error starting services:', err);
+          process.exit(1);
+        });
+    } catch (err) {
+      this.app.log.error(err);
+      process.exit(1);
+    }
+  }
+
+  private setupGracefulShutdown() {
+    process.on('SIGINT', async () => {
+      this.app.log.info(App.SHUTTING_DOWN_MESSAGE);
+      try {
+        await KafkaUtils.disconnectFromKafka(KafkaEssentials.kafka, this.consumers, this.producers, this.app);
+        await this.app.close();
+        this.app.log.info(App.SERVER_CLOSED_MESSAGE);
+        process.exit(0);
+      } catch (err) {
+        this.app.log.error(err);
+        process.exit(1);
       }
     });
-    await Promise.all([
-      KafkaEssentials.connectToKafka().then(() => {
-        registerHttpRoutes().then(() => {
-          AppServer.startFastify(app, PORT)
-        }).catch((err) => {
-          app.log.error('Error registering routes:', err);
-          process.exit(1);
-        })
-      }).catch((err) => {
-        app.log.error(`Error Connecting Kafka: ${err}`);
-        process.exit(1);
-      }),
-    ])
-      .then(() => {
-        app.log.info('All services started successfully');
-      })
-      .catch((err) => {
-        app.log.error('Error starting services:', err);
-        process.exit(1);
-      });
-  } catch (err) {
-    app.log.error(err);
-    process.exit(1);
   }
-};
-
-initialize();
-
-process.on('SIGINT', async () => {
-  app.log.info(SHUTTING_DOWN_MESSAGE);
-  try {
-    await KafkaUtils.disconnectFromKafka(KafkaEssentials.kafka, consumers, producers, app);
-    await app.close();
-    app.log.info(SERVER_CLOSED_MESSAGE);
-    process.exit(0);
-  } catch (err) {
-    app.log.error(err);
-    process.exit(1);
-  }
-});
-
-function registerHttp(httpInstance: InboundSyns<any, any, any, any>, httpRoutes: HttpBase) {
-
 }
+
+new App();
